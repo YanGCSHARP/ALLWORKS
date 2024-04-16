8cad8b09881cf5644dc186b769830d7240bb66da
ABOBA <ferlik1988@gmail.com>
2024-04-16 22:06:59 +0300

    first

---
+++ C#/WK/WORKSPACE/Program.cs
@@ -0,0 +1,9 @@
+﻿
+DoOp(10,5,add);
+cacl mk = add;
+Console.WriteLine(mk(5, 8));
+
+ void DoOp(int x, int y, cacl op) => Console.WriteLine(op(x, y));
+int substract(int x, int y) => x * y;
+int add(int x, int y) => x + y;
+delegate int cacl(int x,int y);
